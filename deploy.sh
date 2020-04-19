2618991acc1080ed3805610ba7b6f5c4c8d2cac0
Charaf Dellal <charaf.dellal@hotmail.com>
2020-04-19 22:24:26 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:sharaf-deen/sharaf-deen.github.io.git master
+git push -f git@github.com:sharaf-deen/myfirst-app-vuejs.git master:gh-pages
 
 cd -
