d01ad4da58d9036d9d92a7806fee3e2be24b4924
sharaf-deen <63205779+sharaf-deen@users.noreply.github.com>
2020-05-04 17:46:54 +0200

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAPkjMerPZQARYlWDx4xUvLPXJbGTESNO4",
-    authDomain: "quiz-app-vue.firebaseapp.com",
-    databaseURL: "https://quiz-app-vue.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "quiz-app-vue",
     storageBucket: "quiz-app-vue.appspot.com",
-    messagingSenderId: "952810765588",
-    appId: "1:952810765588:web:c46cb259e7a57d027e9601"
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
